3c5c41775c3b155a4817b8156855265135f09a04
Renaud <renaudgdumont@gmail.com>
2024-08-31 23:09:32 -0400

    do some simulation

--- packages/client/main.go
+++ packages/client/main.go
@@ -6,8 +6,16 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"sync"
+	"time"
 
 	"github.com/arr-n-d/gns"
+)
+
+const (
+	NumPlayers        = 100
+	MessagesPerPlayer = 150 // 5 seconds worth of messages at 30 per second
+	TotalDuration     = 5 * time.Second
 )
 
 func StatusCallBackChanged(info *gns.StatusChangedCallbackInfo) {
@@ -17,22 +25,7 @@ func StatusCallBackChanged(info *gns.StatusChangedCallbackInfo) {
 
 	case gns.ConnectionStateConnected:
 		fmt.Println("Connected")
-
-		baseMessage := "Hello, world! Sequence: "
-
-		for i := 1; i <= 5000; i++ {
-			// Create the message with a sequence number
-			message := baseMessage + strconv.Itoa(i)
-
-			// Send the message
-			_, res := info.Conn().SendMessage([]byte(message), gns.SendReliable)
-
-			if res != gns.ResultOK {
-				fmt.Println("Issue fault")
-			}
-		}
-
-		fmt.Println("Sent data")
+		runSimulation(info.Conn())
 
 	case gns.ConnectionStateProblemDetectedLocally:
 		info.Conn().Close(gns.ConnectionEndAppExceptionGeneric, "", false)
@@ -73,4 +66,47 @@ func main() {
 	// }
 	// defer c.Close(gns.ConnectionEndAppExceptionMin, "False", false)
 
+}
+
+func runSimulation(connection gns.Connection) {
+	var wg sync.WaitGroup
+
+	fmt.Printf("Starting simulation with %d players for %v\n", NumPlayers, TotalDuration)
+
+	startTime := time.Now()
+
+	for i := 1; i <= NumPlayers; i++ {
+		wg.Add(1)
+		go simulatePlayer(i, connection, &wg)
+	}
+
+	wg.Wait()
+
+	duration := time.Since(startTime)
+	totalMessages := NumPlayers * MessagesPerPlayer
+	messagesPerSecond := float64(totalMessages) / duration.Seconds()
+
+	fmt.Printf("Simulation completed in %v\n", duration)
+	fmt.Printf("Total messages sent: %d\n", totalMessages)
+	fmt.Printf("Average messages per second: %.2f\n", messagesPerSecond)
+}
+
+func simulatePlayer(playerID int, connection gns.Connection, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	baseMessage := fmt.Sprintf("Player %d, Sequence: ", playerID)
+
+	for i := 1; i <= MessagesPerPlayer; i++ {
+		message := baseMessage + strconv.Itoa(i)
+
+		_, res := connection.SendMessage([]byte(message), gns.SendReliable)
+		if res != gns.ResultOK {
+			fmt.Printf("Player %d: Issue fault on message %d\n", playerID, i)
+		}
+
+		// Sleep to simulate 30 messages per second
+		time.Sleep(TotalDuration / MessagesPerPlayer)
+	}
+
+	fmt.Printf("Player %d finished sending %d messages\n", playerID, MessagesPerPlayer)
 }
